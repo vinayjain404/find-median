83358d4c59448ae40be3126062d8e1f63fae0583
vinay jain <vinayjain@vinayjain-macbookair.local>
2012-11-24 03:09:42 -0800

    renamed the variables for better understanding

--- find_median.py
+++ find_median.py
@@ -31,33 +31,33 @@ def get_number_of_elements(filename):
 def split_input_data(filename, block_size):
 	num_count = 0
 	num_data = []
-	input_files = []
-	output_file = None
-
+	input_filenames = []
+	input_file = None
 	file = open(filename)
+
 	# skip the first line as it contains the number of elements
 	file.readline()
 
 	for num in file:
 		if (num_count % block_size) == 0:
-			if output_file:
+			if input_file:
 				for sorted_num in sorted(num_data):
-					output_file.write('%s\n' %sorted_num)
-				output_file.close()
+					input_file.write('%s\n' %sorted_num)
+				input_file.close()
 				num_data = []
 
 			filename = os.path.join(DATA_DIRECTORY, 'input%s.data' %(num_count/block_size))
-			input_files.append(filename)
-			output_file = open(filename, 'w')
+			input_filenames.append(filename)
+			input_file = open(filename, 'w')
 
 		num_data.append(int(num.strip()))
 		num_count += 1
 
 	# store the numbers in the last file
 	for sorted_num in sorted(num_data):
-		output_file.write('%s\n' %sorted_num)
-	output_file.close()
-	return input_files
+		input_file.write('%s\n' %sorted_num)
+	input_file.close()
+	return input_filenames
 
 if __name__ == "__main__":
 	find_median(sys.argv[1])
